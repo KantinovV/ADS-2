3225218be32cb3ed1c6ff273111c71a1470bbff5
KantinovV <125280143+KantinovV@users.noreply.github.com>
2023-03-07 14:48:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,33 +3,38 @@
 #include "alg.h"
 using namespace std;
 double pown(double value, uint16_t n) {
- if (n == 1) {
-  return value;
- }
+ double pow = value;
  if (n == 0) {
   return 1;
  }
- return(value*pown(value, n-1));
+ for (uint64_t i = 1; i < n; i++) {
+  value = value * pow;
+ }
+ return value;
 }
 uint64_t fact(uint16_t n)  {
- if (n == 0) {
+  uint64_t count = n, res = n;;
+ if (res <= 1) {
   return 1;
  }
- return n * fact(n - 1);
+ while (count != 1) {
+  res = res * (count - 1);
+  count--;
+ }
+ return res;
 }
 double calcItem(double x, uint16_t n) {
  return(pown(x, n) / fact(n));
 }
 double expn(double x, uint16_t count) {
- uint16_t res = 1;
- for (int i = 0; i < count; i++) {
-  res = res + pown(x, i) / fact(i);
- }
+ double res = 1;
+ for (uint16_t i = 1; i <= count; i++)
+  res += calcItem(x, i);
  return res;
 }
 double sinn(double x, uint16_t count) {
  double res = 0, res1 = 0;
- for (int i = 1; i <= count; i++) {
+ for (uint16_t i = 1; i <= count; i++) {
   res1 = pown(-1, i - 1) * pown(x, 2 * i - 1) / fact(2 * i - 1);
   res = res + res1;
  }
@@ -37,9 +42,12 @@ double sinn(double x, uint16_t count) {
 }
 double cosn(double x, uint16_t count) {
  double res = 0, res1 = 0;
- for (int i = 1; i <= count; i++) {
+ for (uint16_t i = 1; i <= count; i++) {
   res1 = pown(-1, i - 1) * pown(x, 2 * i - 2) / fact(2 * i - 2);
   res = res + res1;
  }
  return res;
+}
+int main() {
+ cout << expn(1.0, 2) << endl;
 }
